b38a691dc4e73c1a631d419b11aed5fc44073492
judenilson <judenilson@hotmail.com>
2022-12-12 12:21:14 -0300

    no time data

--- rtc/src/main.c
+++ rtc/src/main.c
@@ -1,8 +1,44 @@
+// #include "stm32f4xx.h"
 #include "stm32f407xx.h"
 #include <stdio.h>
 #include <string.h>
 #include "stdlib.h"
 #include "stdarg.h"
+
+void Configure_Clock(void)
+{
+	//Parâmetros do PLL principal
+	#define PLL_M	4
+	#define PLL_N	168
+	#define PLL_P	2
+	#define PLL_Q	7
+
+	//Reseta os registradores do módulo RCC para o estado inicial
+	RCC->CIR = 0;				//desabilita todas as interrupções de RCC
+	RCC->CR |= RCC_CR_HSION;	//liga o oscilador HSI
+	RCC->CFGR = 0;				//reseta o registrador CFGR
+	//Desliga HSE, CSS e o PLL e o bypass de HSE
+	RCC->CR &= ~(RCC_CR_HSEON | RCC_CR_CSSON |
+			   RCC_CR_PLLON | RCC_CR_HSEBYP);
+	RCC->PLLCFGR = 0x24003010;	//reseta o registrador PLLCFGR
+
+	//Configura a fonte de clock (HSE), os parâmetros do PLL,
+	//prescalers dos barramentos AHB, APB
+	RCC->CR |= RCC_CR_HSEON;				//habilita HSE
+	while(!((RCC->CR) & RCC_CR_HSERDY));	//espera HSE ficar pronto
+    RCC->CFGR |= 0x9400;	//HCLK = SYSCLK/1, PCLK2 = HCLK/2, PCLK1 = HCLK/4
+
+    //Configura a fonte de clock e os parâmetros do PLL principal
+    RCC->PLLCFGR = PLL_M | (PLL_N << 6) | (((PLL_P >> 1) -1) << 16) |
+                   (0x400000)           | (PLL_Q << 24);
+
+    RCC->CR |= RCC_CR_PLLON;			//habilita o PLL
+    while(!(RCC->CR & RCC_CR_PLLRDY));	//espera o PLL ficar pronto verificando a flag PLLRDY
+
+    //Seleciona o PLL como fonte de SYSCLK e espera o PLL ser a fonte de SYSCLK
+    RCC->CFGR |= 0x2;
+    while((RCC->CFGR & 0xC) != 0x8);
+}
 
 void Delay_Start(void)
 {
@@ -16,23 +52,23 @@ void Delay_Start(void)
 
 void USART1_Init(void)
 {
-    // configuração da USART1
-    RCC->APB2ENR |= RCC_APB2ENR_USART1EN; // habilita o clock da USART1
-    USART1->BRR = 45.5625;                // ajusta baud rate para 115200 bps (frequência de clock de 84MHz)
-    // clkPer/(baudRx_16bit)=84MHZ/115200 = 729 = 0x2D9
-    // O estado default do registrador USART1->CR1 garante:
-    // 1 stop bit, 8 bits de dados, sem bit de paridade,
-    // oversampling de 16 amostras em cada bit
-    USART1->CR1 |= (USART_CR1_TE | USART_CR1_UE); // habilita o trasmissor e a USART1
+	//configuração da USART1
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;				//habilita o clock da USART1
+	USART1->BRR = 364.58;									//ajusta baud rate para 1 Mbps (frequência de clock de 84MHz)
+	//O estado default do registrador USART1->CR1 garante:
+	//1 stop bit, 8 bits de dados, sem bit de paridade,
+	//oversampling de 16 amostras em cada bit
+	USART1->CR1 |= (USART_CR1_TE | USART_CR1_RE |		//habilita o trasmissor e o receptor
+					USART_CR1_RXNEIE | USART_CR1_UE);	//habilita interrupção de RX e a USART1
 
-    // Habilita a interrupção da USART1 no NVIC
-    //  NVIC_SetPriority(USART1_IRQn, 0);		//seta a prioridade da USART1
-    //  NVIC_EnableIRQ(USART1_IRQn);			//habilita a interrupção da USART1
+	//Habilita a interrupção da USART1 no NVIC
+	NVIC_SetPriority(USART1_IRQn, 0);		//seta a prioridade da USART1
+	NVIC_EnableIRQ(USART1_IRQn);			//habilita a interrupção da USART1
 
-    // Configuração dos pinos PA9 (TX) e PA10(RX)
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;            // habilita o clock do GPIOA
-    GPIOA->MODER |= (0b10 << 20) | (0b10 << 18);    // pinos PA10 e PA9 como função alternativa
-    GPIOA->AFR[1] |= (0b0111 << 8) | (0b0111 << 4); // função alternativa 7 (USART1)
+	//Configuração dos pinos PA9 (TX) e PA10(RX)
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;			//habilita o clock do GPIOA
+	GPIOA->MODER |= (0b10 << 20) | (0b10 << 18) ;	//pinos PA10 e PA9 como função alternativa
+	GPIOA->AFR[1] |= (0b0111 << 8) | (0b0111 << 4);	//função alternativa 7 (USART1)
 }
 
 void Delay_ms(uint32_t delay)
@@ -118,6 +154,7 @@ typedef struct
         segundosUnidade, // especifica os segundos
         segundosDezena;  // especifica os segundos
 } RTC_FullCalendarTypeDef;
+RTC_FullCalendarTypeDef calendar;
 
 void RTC_SetCalendar(RTC_CalendarTypeDef *RTC_CalendarStruct)
 {
@@ -129,8 +166,8 @@ void RTC_SetCalendar(RTC_CalendarTypeDef *RTC_CalendarStruct)
     RTC->WPR = 0xCA;
     RTC->WPR = 0x53;
     RTC->ISR |= RTC_ISR_INIT; // entra no modo de inicialização
-    while (!(RTC->ISR & RTC_ISR_INITF))
-        ; // aguarda o modo de inicialização
+    while (!(RTC->ISR & RTC_ISR_INITF)); // aguarda o modo de inicialização
+
     // Escolha do formato da hora
     if (RTC_CalendarStruct->formato)
         RTC->CR |= RTC_CR_FMT; // formato AM/PM
@@ -166,20 +203,37 @@ void RTC_SetCalendar(RTC_CalendarTypeDef *RTC_CalendarStruct)
 
 void getValueRTC(RTC_FullCalendarTypeDef *calendarNow)
 {
-    calendarNow->segundosUnidade = RTC->TR & RTC_TR_SU;
-    calendarNow->segundosDezena = RTC->TR & RTC_TR_ST;
-    calendarNow->minutosUnidade = RTC->TR & RTC_TR_MNU;
-    calendarNow->minutosDezena = RTC->TR & RTC_TR_MNT;
-    calendarNow->horasUnidade = RTC->TR & RTC_TR_HU;
-    calendarNow->horasDezena = RTC->TR & RTC_TR_HT;
-    calendarNow->dia = RTC->DR & RTC_DR_WDU;
-    calendarNow->dataUnidade = RTC->DR & RTC_DR_DU;
-    calendarNow->dataDezena = RTC->DR & RTC_DR_DT;
-    calendarNow->mesUnidade = RTC->DR & RTC_DR_MU;
-    calendarNow->mesDezena = RTC->DR & RTC_DR_MT;
-    calendarNow->anoUnidade = RTC->DR & RTC_DR_YU;
-    calendarNow->anoDezena = RTC->DR & RTC_DR_YT;
+	//RTC_DR DIAS
+    uint8_t dia = RTC->DR & RTC_DR_WDU;
+    uint8_t dezena_data = RTC->DR & RTC_DR_DT;
+    uint8_t unidade_data = RTC->DR & RTC_DR_DU;
+    uint8_t dezena_mes =  RTC->DR & RTC_DR_MT;
+    uint8_t unidade_mes =  RTC->DR & RTC_DR_MU;
+    uint8_t dezena_ano = RTC->DR & RTC_DR_YT;
+    uint8_t unidade_ano = RTC->DR & RTC_DR_YU;
+    //RTC_TR HORAS
+    uint8_t dezena_hora = RTC->TR & RTC_TR_HT;
+    uint8_t unidade_hora = RTC->TR & RTC_TR_HU;
+    uint8_t dezena_minuto = RTC->TR & RTC_TR_MNT;
+    uint8_t unidade_minuto = RTC->TR & RTC_TR_MNU;
+    uint8_t dezena_segundo = RTC->TR & RTC_TR_ST;
+    uint8_t unidade_segundo = RTC->TR & RTC_TR_SU;
+
+    calendarNow->dia = dia;
+    calendarNow->dataDezena = dezena_data;
+    calendarNow->dataUnidade = unidade_data;
+    calendarNow->mesDezena = dezena_mes;
+    calendarNow->mesUnidade = unidade_mes;
+    calendarNow->anoDezena = dezena_ano;
+    calendarNow->anoUnidade = unidade_ano;
+    calendarNow->horasDezena = dezena_hora;
+    calendarNow->horasUnidade = unidade_hora;
+    calendarNow->minutosDezena = dezena_minuto;
+    calendarNow->minutosUnidade = unidade_minuto;
+    calendarNow->segundosDezena = dezena_segundo;
+    calendarNow->segundosUnidade = unidade_segundo;
 }
+
 char diaDaSemana[8][15] = {"proibido", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado", "domingo"};
 char mesD[11][10] = {"proibido", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro"};
 char mesU[3][10] = {"outubro", "novembro", "dezembro"};
@@ -199,6 +253,7 @@ void mes(int d, int u)
 
 int main()
 {
+	Configure_Clock();
     Delay_Start();
     USART1_Init();
     RTC_Config();
@@ -210,16 +265,14 @@ int main()
 
     // inicializando a estrutura do calendário
     RTC_Calendar.formato = 0;  // definição do formato da hora (24h)
-    RTC_Calendar.dia = 2;      // definição do dia (terça-feira)
-    RTC_Calendar.data = 5;     // definição da data (dia do mês)
+    RTC_Calendar.dia = 1;      // definição do dia (terça-feira)
+    RTC_Calendar.data = 12;     // definição da data (dia do mês)
     RTC_Calendar.mes = 12;     // definição do mês (dezembro)
     RTC_Calendar.ano = 22;     // definição do ano (2025)
-    RTC_Calendar.horas = 21;   // definição da hora
-    RTC_Calendar.minutos = 2;  // definição dos minutos
-    RTC_Calendar.segundos = 0; // definição dos segundos
-    // RTC_SetCalendar(&RTC_Calendar); // atualização do calendário
-
-    RTC_FullCalendarTypeDef calendar;
+    RTC_Calendar.horas = 10;   // definição da hora
+    RTC_Calendar.minutos = 5;  // definição dos minutos
+    RTC_Calendar.segundos = 20; // definição dos segundos
+    RTC_SetCalendar(&RTC_Calendar); // atualização do calendário
 
     int second_status = 0;
 
@@ -249,44 +302,49 @@ int main()
             char concatenar[20];
             char textoEnviar[120] = "Hoje é ";
 
-            strncat(textoEnviar, diaDaSemana[calendar.dia], 12);
-            strcpy(concatenar, ", ");
-            strncat(textoEnviar, concatenar, 2);
-            sprintf(concatenar, "%d", calendar.dataDezena);
-            strncat(textoEnviar, concatenar, 1);
-            sprintf(concatenar, "%d", calendar.dataUnidade);
-            strncat(textoEnviar, concatenar, 1);
-            strcpy(concatenar, " de ");
-            strncat(textoEnviar, concatenar, 4);
-            sprintf(concatenar, "%d", calendar.dataUnidade);
-            strncat(textoEnviar, concatenar, 1);
-            strncat(textoEnviar, mesNow, 9);
-            strcpy(concatenar, " de 20");
-            strncat(textoEnviar, concatenar, 6);
-            sprintf(concatenar, "%d", calendar.anoDezena);
-            strncat(textoEnviar, concatenar, 1);
-            sprintf(concatenar, "%d", calendar.anoUnidade);
-            strncat(textoEnviar, concatenar, 1);
-            strcpy(concatenar, ".\nA hora atual é ");
-            strncat(textoEnviar, concatenar, 18);
-            sprintf(concatenar, "%d", calendar.horasDezena);
-            strncat(textoEnviar, concatenar, 1);
-            sprintf(concatenar, "%d", calendar.horasUnidade);
-            strncat(textoEnviar, concatenar, 1);
-            strcpy(concatenar, ":");
-            strncat(textoEnviar, concatenar, 1);
-            sprintf(concatenar, "%d", calendar.minutosDezena);
-            strncat(textoEnviar, concatenar, 1);
-            sprintf(concatenar, "%d", calendar.minutosUnidade);
-            strncat(textoEnviar, concatenar, 1);
-            strcpy(concatenar, ":");
-            strncat(textoEnviar, concatenar, 1);
-            sprintf(concatenar, "%d", calendar.segundosDezena);
-            strncat(textoEnviar, concatenar, 1);
-            sprintf(concatenar, "%d", calendar.segundosUnidade);
-            strncat(textoEnviar, concatenar, 1);
+        //    sprintf(concatenar, "%d", calendar.dia);
+        //     sendString(concatenar);
 
-            sendString(textoEnviar);
+           strncat(textoEnviar, diaDaSemana[calendar.dia], 15);
+           strcpy(concatenar, ", ");
+           strncat(textoEnviar, concatenar, 2);
+           sprintf(concatenar, "%d", calendar.dataDezena);
+           strncat(textoEnviar, concatenar, 1);
+           sprintf(concatenar, "%d", calendar.dataUnidade);
+           strncat(textoEnviar, concatenar, 1);
+           strcpy(concatenar, " de ");
+           strncat(textoEnviar, concatenar, 4);
+           mes(calendar.mesDezena, calendar.mesUnidade);
+           sprintf(concatenar, "%d", mesNow);
+           strncat(textoEnviar, mesNow, 9);
+           strcpy(concatenar, " de 20");
+           strncat(textoEnviar, concatenar, 6);
+           sprintf(concatenar, "%d", calendar.anoDezena);
+           strncat(textoEnviar, concatenar, 1);
+           sprintf(concatenar, "%d", calendar.anoUnidade);
+           strncat(textoEnviar, concatenar, 1);
+           strcpy(concatenar, ".\nA hora atual é ");
+           strncat(textoEnviar, concatenar, 18);
+           sprintf(concatenar, "%d", calendar.horasDezena);
+           strncat(textoEnviar, concatenar, 1);
+           sprintf(concatenar, "%d", calendar.horasUnidade);
+           strncat(textoEnviar, concatenar, 1);
+           strcpy(concatenar, ":");
+           strncat(textoEnviar, concatenar, 1);
+           sprintf(concatenar, "%d", calendar.minutosDezena);
+           strncat(textoEnviar, concatenar, 1);
+           sprintf(concatenar, "%d", calendar.minutosUnidade);
+           strncat(textoEnviar, concatenar, 1);
+           strcpy(concatenar, ":");
+           strncat(textoEnviar, concatenar, 1);
+           sprintf(concatenar, "%d", calendar.segundosDezena);
+           strncat(textoEnviar, concatenar, 1);
+           sprintf(concatenar, "%d", calendar.segundosUnidade);
+           strncat(textoEnviar, concatenar, 1);
+           strcpy(concatenar, "\n");
+           strncat(textoEnviar, concatenar, 1);
+
+           sendString(textoEnviar);
         }
     }
 }
